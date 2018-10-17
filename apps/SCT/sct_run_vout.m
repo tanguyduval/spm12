9a9d1e708c6d84f91b78a23f88efb908316161cd
Tanguy Duval <duvaltanguy@gmail.com>
2018-10-17 17:34:45 +0200

    remove old system calls

--- apps/SCT/sct_run_vout.m
+++
@@ -1,5 +0,0 @@
-function dep = sct_run_vout(job)
-dep = cfg_dep;
-dep(1).sname      = sprintf('output file');
-dep(1).src_output = substruct('.','o');
-dep(1).tgt_spec   = cfg_findspec({{'class','cfg_files', 'strtype','e'}});
