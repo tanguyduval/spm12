d8d423a5ca0020d4e95fce6275f7b31992da6c76
Tanguy Duval <duvaltanguy@gmail.com>
2019-01-23 12:11:58 +0100

    Add Boutiques installer

--- install.m
+++ install.m
@@ -7,7 +7,6 @@ addpath(fullfile(spm('Dir'),'matlabbatch','cfg_basicio'));
 addpath(fullfile(spm('Dir'),'config'));
 addpath(genpath(fullfile(spm('Dir'),'external')));
 addpath(genpath(fullfile(spmpath,'apps')));
-cfg_util('initcfg')
 
 % Default app folder location
 defaultappfname = fullfile(spmpath,'apps_savepath.txt');
@@ -16,3 +15,12 @@ if ~exist(defaultappfname,'file')
     fprintf(fid,'%s',fullfile(spmpath,'apps'));
     fclose(fid);
 end
+
+% Add Boutiques
+directory = textread(defaultappfname,'%s');
+if exist(fullfile(directory{1},'Boutiques'),'dir')
+    copyfile(which('cfg_mlbatch_appcfg_Boutiques.m'),fullfile(directory{1},'Boutiques','cfg_mlbatch_appcfg.m'))
+end
+
+% init 
+cfg_util('initcfg')
